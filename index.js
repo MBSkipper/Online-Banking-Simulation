c01f81766bae6c418d219bdd91cb52966cb08d8a
MBSkipper <trawler22@gmail.com>
2025-11-30 08:17:48 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+/*Requirements
+    - Account Balance
+    -Transfer funds
+        - Transfer in (Credit)
+        - Transfer out (Debit)
+    
+    
+    function toggleAccountNo {
+    const eyeBtn = getElementById("see-account-no")
+    }
+    */
